802ecc986b2823452e1d23c342edbb80643d7c57
Christian Haas <christian.haas@sevensuns.at>
2019-02-17 19:38:06 +0100

    outline of standard demo

---
+++ pkg/demo/Run.go
@@ -0,0 +1,107 @@
+package demo
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/inkyblackness/imgui-go"
+)
+
+// Platform covers mouse/keyboard/gamepad inputs, cursor shape, timing, windowing.
+type Platform interface {
+	// ShouldStop is regularly called as the abort condition for the render loop.
+	ShouldStop() bool
+	// ProcessEvents is called once per render loop to dispatch any pending events.
+	ProcessEvents()
+	// DisplaySize returns the dimension of the display.
+	DisplaySize() [2]float32
+	// FramebufferSize returns the dimension of the framebuffer.
+	FramebufferSize() [2]float32
+	// NewFrame marks the begin of a render pass.
+	NewFrame()
+	// PostRender marks the completion of one render pass. Typically this causes the display buffer to be swapped.
+	PostRender()
+}
+
+// Renderer covers rendering imgui draw data.
+type Renderer interface {
+	// PreRender causes the display buffer to be prepared for new output.
+	PreRender(displaySize [2]float32, clearColor [4]float32)
+	// Render draws the provided imgui draw data.
+	Render(displaySize [2]float32, framebufferSize [2]float32, drawData imgui.DrawData)
+}
+
+// Run implements the main render loop of the demo. It returns when the platform signals to stop.
+// This demo application shows some basic features of ImGui, as well as exposing the standard demo window.
+func Run(p Platform, r Renderer) {
+	showDemoWindow := false
+	clearColor := [4]float32{0.0, 0.0, 0.0, 1.0}
+	f := float32(0.0)
+	counter := 0
+	showAnotherWindow := false
+
+	for !p.ShouldStop() {
+		p.ProcessEvents()
+
+		// Signal start of a new frame
+		p.NewFrame()
+		imgui.NewFrame()
+
+		// 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()!
+		// You can browse its code to learn more about Dear ImGui!).
+		if showDemoWindow {
+			imgui.ShowDemoWindow(&showDemoWindow)
+		}
+
+		// 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
+		{
+			imgui.Begin("Hello, world!") // Create a window called "Hello, world!" and append into it.
+
+			imgui.Text("This is some useful text.") // Display some text
+
+			imgui.Checkbox("Demo Window", &showDemoWindow) // Edit bools storing our window open/close state
+			imgui.Checkbox("Another Window", &showAnotherWindow)
+
+			imgui.SliderFloat("float", &f, 0.0, 1.0) // Edit one float using a slider from 0.0f to 1.0f
+			// TODO add example of ColorEdit3 for clearColor
+
+			if imgui.Button("Button") { // Buttons return true when clicked (most widgets return true when edited/activated)
+				counter++
+			}
+			imgui.SameLine()
+			imgui.Text(fmt.Sprintf("counter = %d", counter))
+
+			// TODO add text of FPS based on IO.Framerate()
+
+			imgui.End()
+		}
+
+		// 3. Show another simple window.
+		if showAnotherWindow {
+			// Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+			imgui.BeginV("Another window", &showAnotherWindow, 0)
+
+			imgui.Text("Hello from another window!")
+			if imgui.Button("Close Me") {
+				showAnotherWindow = false
+			}
+			imgui.End()
+		}
+
+		// Rendering
+		imgui.Render()
+
+		displaySize := p.DisplaySize()
+		framebufferSize := p.FramebufferSize()
+
+		r.PreRender(displaySize, clearColor)
+		// A this point, the application could perform its own rendering...
+		// app.RenderScene()
+
+		r.Render(displaySize, framebufferSize, imgui.RenderedDrawData())
+		p.PostRender()
+
+		// sleep to avoid 100% CPU usage for this demo
+		<-time.After(time.Millisecond * 25)
+	}
+}
