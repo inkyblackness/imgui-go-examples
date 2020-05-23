00a7249e4d65d6da9e7209a6f416cb43dac4405e
Christian Haas <christian.haas@sevensuns.at>
2020-05-23 13:01:44 +0200

    added package documentation

---
+++ internal/demo/doc.go
@@ -0,0 +1,5 @@
+// Package demo contains the ported code of imgui_demo.cpp .
+// This package is intended both to showcase what ImGui can provide, as well as
+// what has actually been wrapped.
+// The visible part should be matched as closely as possible - the code only in spirit.
+package demo
